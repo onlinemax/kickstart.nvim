e0dffdb0976cd5307db22b2556443ade1e2a0db4
onlinemax <onlinepromax12@gmail.com>
2024-12-30 23:28:32 -0500

    Override the default override for shiftwidth and tabstop for html files

--- lua/onlinemax/general.lua
+++ lua/onlinemax/general.lua
@@ -7,3 +7,11 @@ vim.api.nvim_set_option_value('clipboard', '', { scope = 'local' })
 -- Change the shiftwidth and the tabstop to 4 globally
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+-- Override the shiftwidth and tabstop for html to 4
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = '*.html',
+  callback = function()
+    vim.opt.shiftwidth = 2
+    vim.opt.tabstop = 2
+  end,
+})
