6e8bf01a9f1a056e093e19d9175b82e8cecc9961
onlinemax <onlinepromax12@gmail.com>
2024-12-30 23:21:55 -0500

    Change the default shiftwidth and tabstop to 4

--- lua/onlinemax/general.lua
+++ lua/onlinemax/general.lua
@@ -2,5 +2,8 @@
 require('Comment').setup()
 -- Some general settings because i really need it:
 
--- Don't change my clipbard
+-- Don't change my clipboard
 vim.api.nvim_set_option_value('clipboard', '', { scope = 'local' })
+-- Change the shiftwidth and the tabstop to 4 globally
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
